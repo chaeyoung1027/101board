a16677f48756551a04624b93d12cbdeed1ba1d35
chaeyoung1027 <83990991+chaeyoung1027@users.noreply.github.com>
2021-05-25 09:16:24 +0900

    if문을 switch로 바꾸기 완성

--- 07-02스위치.c
+++ 07-02스위치.c
@@ -11,17 +11,23 @@ void main(void)
 		//if(input==1)
 	case 1:
 		printf("피카츄\n");
+		break;
 		//else if(input==2)
 	case 2:
 		printf("라이츄\n");
+		break;
 	case 3:
 		printf("파이리\n");
+		break;
 	case 4:
-			printf("꼬부기\n");
+		printf("꼬부기\n");
+		break;
 	case 5:
-			printf("버터풀\n");
+		printf("버터풀\n");
+		break;
 	case 6:
 			printf("야도란\n");
+		break;
 		//else
 	default:
 		printf("잘못 입력하였습니다.\n");
