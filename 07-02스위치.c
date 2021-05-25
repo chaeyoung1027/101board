98fb5f8ba8b1f2f5eaa13726082e62136383dad5
chaeyoung1027 <83990991+chaeyoung1027@users.noreply.github.com>
2021-05-25 09:14:17 +0900

    if문을 sitch로 바꾸기(아래코드가 계속 실행되는 문제가 있음)

--- 07-02스위치.c
+++ 07-02스위치.c
@@ -6,17 +6,38 @@ void main(void)
 	printf("숫자를 입력하세요(1~8까지) : ");
 	scanf("%d", &input);
 
+	switch (input)
+	{
+		//if(input==1)
+	case 1:
+		printf("피카츄\n");
+		//else if(input==2)
+	case 2:
+		printf("라이츄\n");
+	case 3:
+		printf("파이리\n");
+	case 4:
+			printf("꼬부기\n");
+	case 5:
+			printf("버터풀\n");
+	case 6:
+			printf("야도란\n");
+		//else
+	default:
+		printf("잘못 입력하였습니다.\n");
+	}
+
 	if (input == 1)
 		printf("피카츄\n");
 	else if (input == 2)
 		printf("라이츄\n");
-	else if (input == 2)
+	else if (input == 3)
 		printf("파이리\n");
-	else if (input == 2)
+	else if (input == 4)
 		printf("꼬부기\n");
-	else if (input == 2)
+	else if (input == 5)
 		printf("버터풀\n");
-	else if (input == 2)
+	else if (input == 6)
 		printf("야도란\n");
 	else
 		printf("잘못 입력하셨습니다.");
