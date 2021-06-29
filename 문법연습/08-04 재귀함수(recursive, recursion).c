e0ccdebc0958feed7e4e25c2bdab2234c9f0b192
chaeyoung1027 <83990991+chaeyoung1027@users.noreply.github.com>
2021-06-29 10:14:25 +0900

    recusieve 재귀함수 완료

--- 문법연습/08-04 재귀함수(recursive, recursion).c
+++ 문법연습/08-04 재귀함수(recursive, recursion).c
@@ -4,13 +4,13 @@ int factorial(int num) //num은 공간
 {
 	if (num == 1)
 		return 1;
+	return num * factorial(num - 1);
 }
 
 
 void main(void)//함수의 구성요소 : 함수 이름과 매개변수와 반환형
 {
-	int val = 1;
-	int a;
+	int val = 3;
 	a = factorial(val); //argument
 	printf("%d 팩토리얼은 %d\n", val, a);
 }
