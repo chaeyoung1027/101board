a9b09c23d9c04a214b2ee8ac94380c89f91dffb9
chaeyoung1027 <83990991+chaeyoung1027@users.noreply.github.com>
2021-08-31 08:54:57 +0900

    여러과목 추가

--- 문법연습/성적처리01.c
+++ 문법연습/성적처리01.c
@@ -2,24 +2,70 @@
 
 void main(void)
 {
-	printf("점수를 입력하시오 : ");
-	int score;
-	scanf("%d", &score);
+	printf("씨언어 점수를 입력하시오 : ");
+	int c_score;
+	scanf("%d", &c_score);
 
-	if (90 <= score & score <= 100)
+	if (90 <= c_score && c_score <= 100)
 	{
 		printf("A");
 	}
-	else if (80 <= score & score <= 89)
+	else if (80 <= c_score && c_score <= 89)
 	{
 		printf("B");
 	}
-	else if (70 <= score  & score <= 79)
+	else if (70 <= c_score && c_score <= 79)
 	{
 		printf("C");
 	}
-	else if (69 >= score)
+	else if (60 <= c_score && c_score < 70)
 	{
 		printf("D");
 	}
+	else printf("E");
+		printf("\n");
+		printf("자바 점수를 입력하시오 : ");
+		int java_score;
+		scanf("%d", &java_score);
+
+		if (90 <= java_score && java_score <= 100)
+		{
+			printf("A");
+		}
+		else if (80 <= java_score && java_score <= 89)
+		{
+			printf("B");
+		}
+		else if (70 <= java_score && java_score <= 79)
+		{
+			printf("C");
+		}
+		else if (60 <= java_score && java_score < 70)
+		{
+			printf("D");
+		}
+		else printf("E");
+		printf("\n");
+			printf("컴퓨터구조 점수를 입력하시오 : ");
+			int ca_score;
+			scanf("%d", &ca_score);
+
+			if (90 <= ca_score && ca_score <= 100)
+			{
+				printf("A");
+			}
+			else if (80 <= ca_score && ca_score <= 89)
+			{
+				printf("B");
+			}
+			else if (70 <= ca_score && ca_score <= 79)
+			{
+				printf("C");
+			}
+			else if (60 <= ca_score && ca_score<70)
+			{
+				printf("D");
+			}
+			else printf("E");
+			printf("\n");
 }
