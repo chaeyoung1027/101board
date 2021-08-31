4bc7bb35736a3f374762c469e8f2fdcb33d61c2d
chaeyoung1027 <83990991+chaeyoung1027@users.noreply.github.com>
2021-08-31 10:12:23 +0900

    자료형태를 배열로 전환(refactoring)

--- 문법연습/성적처리01.c
+++ 문법연습/성적처리01.c
@@ -5,27 +5,44 @@ float average(int a, int b, int c);
 
 void main(void)
 {
-	float avg;
-	int C_score;
+	float avg;			// 평균
+	int subject[3];		// 과목
+
 	printf("씨언어 점수를 입력하시오 : ");
-	scanf("%d", &C_score);
-	cal_grade(C_score);
+	scanf("%d", &subject[0]);
+	cal_grade(subject[0]);
 
-	int java_score;
 	printf("자바 점수를 입력하시오 : ");
-	scanf("%d", &java_score);
-	cal_grade(java_score);
+	scanf("%d", &subject[1]);
+	cal_grade(subject[1]);
 
-	int CA_score;
 	printf("컴퓨터구조 점수를 입력하시오 : ");
-	scanf("%d", &CA_score);
-	cal_grade(CA_score);
+	scanf("%d", &subject[2]);
+	cal_grade(subject[2]);
 
-	avg = average(C_score, java_score, CA_score);
+	avg = average(subject[0], subject[1], subject[2]);
+	printf("세 과목의 평균은 %f입니다\n", avg);
 }
 
-//세 점수의 평균을 반환
+// 입력받은 점수(score)에 대한 등급을 출력
+void cal_grade(int score)
+{
+	if (90 <= score && score <= 100)
+		printf("A");
+	else if (80 <= score && score < 90)
+		printf("B");
+	else if (70 <= score && score < 80)
+		printf("C");
+	else if (60 <= score && score < 70)
+		printf("D");
+	else
+		printf("E");
+
+	printf("\n");
+}
+
+// 세 점수의 평균을 반환
 float average(int a, int b, int c)
 {
-	return (float)(a + b + c) / 3.f;
+	return (a + b + c) / 3.f;
 }
