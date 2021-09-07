db713270d600148b3929df4d7b3eaa9961d846ac
chaeyoung1027 <83990991+chaeyoung1027@users.noreply.github.com>
2021-09-07 09:39:46 +0900

    간단하게 W까지만 뽑기

--- 문법연습/예제3.c
+++ 문법연습/예제3.c
@@ -5,6 +5,6 @@ void main(void)
     char str[20] = "Hello World";
     printf("%d\n", sizeof(str));
 
-    for (int i = 0; i < 7; i++)
-        printf("%c", str[i]);
+    str[7] = '\0';
+    printf("%s", str);
 }
