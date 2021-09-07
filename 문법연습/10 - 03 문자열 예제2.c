0315cd73f5ed767cde6da83a371d1a4e65e614a6
chaeyoung1027 <83990991+chaeyoung1027@users.noreply.github.com>
2021-09-07 09:14:22 +0900

    반복문을 효율적으로

--- 문법연습/10 - 03 문자열 예제2.c
+++ 문법연습/10 - 03 문자열 예제2.c
@@ -2,9 +2,12 @@
 
 void main(void)
 {
-	char str[20] = "Hello World";
+	//str뒤 대괄호에 숫자를 넣어주지 않으면 자동으로 넣어줌.
 
-	//단순하게 인덱스 값을 직접 계산함
+	char str[] = "Hello World";
+	printf("%d\n", sizeof(str));
+
+	//문자열의 갯수를 세지 않고 널문자 여부만을 확인 **훨씬 더 효율적
 	for (int i = 0; str[i] != '\0' ; i++)
 		printf("%c", str[i]);
 }
