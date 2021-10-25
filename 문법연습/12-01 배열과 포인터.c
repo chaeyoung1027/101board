9c604904a30353dfc27d0d7b0fde11a5f4b801ea
chaeyoung1027 <83990991+chaeyoung1027@users.noreply.github.com>
2021-10-25 14:11:35 +0900

    배열과 포인터

---
+++ 문법연습/12-01 배열과 포인터.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main(void)
+{
+	int arr[4];
+	printf("arr 0번째 공간의 주소 : %p \n", &arr[0]);
+	printf("arr 배열의 시작 주소 : %p \n", arr);
+	printf("arr 1번째 공간의 주소 : %p \n\n", &arr[1]);
+	char str[4];
+	printf("str 0번째 공간의 주소 : %p \n", &str[0]);
+	printf("str 배열의 시작 주소 : %p \n", str);
+	printf("str 1번째 공간의 주소 : %p \n", &str[1]);
+}
