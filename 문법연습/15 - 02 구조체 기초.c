cdd5c246a48d063f87f7350633a5f044806fa976
chaeyoung1027 <83990991+chaeyoung1027@users.noreply.github.com>
2021-11-30 09:52:27 +0900

    구조체에 typedef 적용하는 방법 1

--- 문법연습/15 - 02 구조체 기초.c
+++ 문법연습/15 - 02 구조체 기초.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 
-struct point {
+typedef struct point {
 	int x;
 	int y;
-};
+}Point;
 
 void main(void)
 {
 	//선언과 동시에 초기화
-	struct point p1 = { 10, 20 };
+	Point p1 = { 10, 20 };
 	//point p2;        에러발생 꼭 struct를 넣어야 함
 	struct point p2;
 	p2.x = 20;
@@ -17,4 +17,5 @@ void main(void)
 	printf("(%d, %d)\n", p1.x, p1.y);
 	printf("(%d, %d)\n", p2.x, p2.y);
 	printf("구조체의 크기는 %d", sizeof(struct point));
+
 }
